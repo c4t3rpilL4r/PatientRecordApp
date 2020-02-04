960541932e5393d8434ff17f5c2ef58ab8b638e6
Raphael de Arce <raphael.dearce@mllrdev.com>
2020-02-04 20:29:27 +0800

    current update

--- PatientRecordApp.UI.Winforms.MDI/FrmPatientRecordApp.cs
+++ PatientRecordApp.UI.Winforms.MDI/FrmPatientRecordApp.cs
@@ -2,50 +2,44 @@
 
 namespace PatientRecordApp.UI.Winforms.MDI
 {
-    public partial class FrmPatientRecordApp : Form
-    {
-        public FrmPatientRecordApp()
-        {
-            InitializeComponent();
-        }
+	public partial class FrmPatientRecordApp : Form
+	{
+		public FrmPatientRecordApp()
+		{
+			InitializeComponent();
+		}
 
-        private void FrmPatientRecordApp_Load(object sender, System.EventArgs e)
-        {
+		private void FrmPatientRecordApp_Load(object sender, System.EventArgs e)
+		{
 
-        }
+		}
 
-        private void patientToolStripMenuItem_Click(object sender, System.EventArgs e)
-        {
-            var form = new FrmAddPatient();
-            OpenForm(form);
-            form.MdiParent = this;
-        }
+		private void patientToolStripMenuItem_Click(object sender, System.EventArgs e)
+		{
+			var form = new FrmAddPatient();
+			OpenForm(form);
+			form.MdiParent = this;
+		}
 
-        private void doctorToolStripMenuItem_Click(object sender, System.EventArgs e)
-        {
-            var form = new FrmAddDoctor();
-            OpenForm(form);
-            form.MdiParent = this;
-        }
+		private void doctorToolStripMenuItem_Click(object sender, System.EventArgs e)
+		{
+			var form = new FrmAddDoctor();
+			OpenForm(form);
+			form.MdiParent = this;
+		}
 
-        private void OpenForm(Form form)
-        {
-            var isOpen = false;
+		private void OpenForm(Form form)
+		{
+			var child = this.MdiChildren.GetType();
 
-            foreach (var frm in this.MdiChildren)
-            {
-                if (frm.Name.Equals(form.Name))
-                {
-                    frm.BringToFront();
-                    isOpen = true;
-                    break;
-                }
-            }
-
-            if (!isOpen)
-            {
-                form.Show();
-            }
-        }
-    }
+			if (child.BaseType != null)
+			{
+				
+			}
+			else
+			{
+				form.Show();
+			}
+		}
+	}
 }
